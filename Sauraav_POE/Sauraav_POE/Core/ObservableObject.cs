b9738d72ffc314c77a842b813d8fc0bed281933d
Sauraav Jayrajh <69242316+Saupernova13@users.noreply.github.com>
2023-06-17 02:20:40 +0200

    Started Building UI
    Grid, Stack Panel, and some definitions added

---
+++ Sauraav_POE/Sauraav_POE/Core/ObservableObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace Sauraav_POE.Core
+{
+    internal class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void onPropertyChanged([CallerMemberName] string name = null)
+        { 
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
