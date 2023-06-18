2f9ac0cb322c029f075dfd273475f49a359a6ce9
Sauraav Jayrajh <69242316+Saupernova13@users.noreply.github.com>
2023-06-18 03:08:11 +0200

    Made Test View
    Test View Made, Program Doesn't Want to Switch views though

---
+++ Sauraav_POE/Sauraav_POE/MVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sauraav_POE.Core;
+
+namespace Sauraav_POE.MVM.ViewModel
+{
+     class MainViewModel: ObservableObject
+    {
+        public RelayCommand homeViewCommand { get; set; }
+        public RelayCommand addRecipeViewCommand { get; set; }
+        public HomeViewModel homeVM { get; set; }
+        public AddRecipeViewModel addRecipeVM { get; set; }
+
+        private object _currentView;
+
+		public object CurrentView
+        {
+			get { return _currentView; }
+			set { _currentView = value; onPropertyChanged(); }
+		}
+
+		public MainViewModel() {
+            homeVM = new HomeViewModel();
+            addRecipeVM = new AddRecipeViewModel();
+            CurrentView = homeVM;
+            homeViewCommand = new RelayCommand(o => { CurrentView = homeVM; });
+            addRecipeViewCommand = new RelayCommand(o => { CurrentView = addRecipeVM; });
+        }
+    }
+}
